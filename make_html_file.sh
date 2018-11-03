c23ec083715bac5bd9f9fcc42380fb0d87a15e1e
Fede Camara Halac <camarafede@gmail.com>
2018-11-03 10:47:13 +0100

    update readme

--- make_html_file.sh
+++ make_html_file.sh
@@ -9,7 +9,7 @@ CAUTO="AUTOMATICALLY GENERATED FROM: 'sh $CMDNAME'"
 EMAIL="FCH226@NYU.EDU"
 DDATE=`date`
 CSS=style.css
-META="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+META="width=device-width,minimum-scale=0.5,maximum-scale=1.5,user-scalable=yes"
 HINTRO="<!--
 
 	$CINTRO
